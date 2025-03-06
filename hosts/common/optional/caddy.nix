1b23ca14ccc3c037eaa94d62a46a15618e2a4cf2
Diego Peña y Lillo <diegopyl1209@gmail.com>
2025-03-05 21:10:29 -0300

    Revert "Update caddy.nix"
    This reverts commit 23870af9385b9123b3e50c51d0db7d213c9f0389.

--- hosts/common/optional/caddy.nix
+++ hosts/common/optional/caddy.nix
@@ -1,10 +1,5 @@
-{ pkgs, ... }:
-{
+{...}: {
   services.caddy = {
     enable = true;
-    package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/duckdns@v0.4.0" ];
-      hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
-    };
   };
 }
