cac1b46d195052e320934d9031c3262663b4c880
Diego Peña y Lillo <diegopyl1209@gmail.com>
2025-03-22 11:34:27 -0300

    fix gnome module ignore gnome enable option

--- hosts/server/home.nix
+++ hosts/server/home.nix
@@ -1,10 +1,14 @@
-{ lib, ... }:
-{
+{lib, ...}: {
   home-manager = {
     graphical.enable = false;
 
     wallpaper = "samurai.jpg";
     colorscheme = "gruvbox";
+
+    hyprland = {
+      enable = false;
+    };
+    gnome.enable = false;
   };
 
   #https://github.com/danth/stylix/pull/912
--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,50 +1,57 @@
-{pkgs, ...}: {
-  services = {
-    xserver = {
-      enable = true;
-      desktopManager.gnome = {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  config = lib.mkIf config.hm.home-manager.gnome.enable {
+    services = {
+      xserver = {
         enable = true;
-      };
-      displayManager.gdm = {
-        # enabled in display-manager.nix
-        autoSuspend = false;
+        desktopManager.gnome = {
+          enable = true;
+        };
+        displayManager.gdm = {
+          # enabled in display-manager.nix
+          autoSuspend = false;
+        };
       };
     };
-  };
 
-  programs.nautilus-open-any-terminal = {
-    enable = true;
-    terminal = "alacritty";
-  };
+    programs.nautilus-open-any-terminal = {
+      enable = true;
+      terminal = "alacritty";
+    };
 
-  environment = {
-    systemPackages = with pkgs; [
-      nautilus
-      nautilus-python
-    ];
+    environment = {
+      systemPackages = with pkgs; [
+        nautilus
+        nautilus-python
+      ];
 
-    gnome.excludePackages = with pkgs; [
-      gnome-console
-      gnome-photos
-      gnome-tour
-      gnome-connections
-      cheese # webcam tool
-      epiphany # web browser
-      geary # email reader
-      evince # document viewer
-      totem # video player
-      yelp # Help view
-      gnome-font-viewer
-      gnome-music
-      gnome-characters
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-      gnome-contacts
-      gnome-initial-setup
-      gnome-shell-extensions
-      gnome-maps
-    ];
+      gnome.excludePackages = with pkgs; [
+        gnome-console
+        gnome-photos
+        gnome-tour
+        gnome-connections
+        cheese # webcam tool
+        epiphany # web browser
+        geary # email reader
+        evince # document viewer
+        totem # video player
+        yelp # Help view
+        gnome-font-viewer
+        gnome-music
+        gnome-characters
+        tali # poker game
+        iagno # go game
+        hitori # sudoku game
+        atomix # puzzle game
+        gnome-contacts
+        gnome-initial-setup
+        gnome-shell-extensions
+        gnome-maps
+      ];
+    };
   };
 }
