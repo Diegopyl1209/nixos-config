aa7880eb263ed2190d4d61ceb71d9a9411103783
Diego Peña y Lillo <diegopyl1209@gmail.com>
2025-03-31 16:40:35 -0300

    update nvim config

--- home/cli/neovim/default.nix
+++ home/cli/neovim/default.nix
@@ -1,14 +1,21 @@
-{...}:{
+{ ... }:
+{
   imports = [
     ./plugins/lualine.nix
     ./plugins/base16.nix
     ./plugins/telescope.nix
     ./plugins/cmp.nix
     ./plugins/lsp.nix
+    ./plugins/treesit.nix
   ];
 
   programs.neovim = {
     enable = true;
+
+    extraLuaConfig = # lua
+      ''
+        vim.g.mapleader = ' '
+      '';
   };
-  
+
 }
---
+++ home/cli/neovim/plugins/treesit.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  programs.neovim = {
+    plugins = [
+      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+    ];
+
+    extraLuaConfig = # lua
+      ''
+        require("nvim-treesitter.configs").setup({
+          highlight = { enable = true },
+          --indent = { enable = true }
+        })
+      '';
+
+  };
+}
