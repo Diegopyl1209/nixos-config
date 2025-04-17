0a8fb600f2782ecd1502dd99a39244c527f2490c
Diego Peña y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2025-04-16 22:59:51 -0400

    replace doom emacs with vanilla emacs

--- home/desktop/emacs/default.nix
+++ home/desktop/emacs/default.nix
@@ -1,9 +1,5 @@
+{ pkgs, ... }:
 {
-  inputs,
-  pkgs,
-  config,
-  ...
-}: {
   services.emacs = {
     enable = true;
     client.enable = true;
@@ -12,40 +8,36 @@
 
   programs.emacs = {
     enable = true;
-    package = (pkgs.emacs.pkgs.withPackages (epkgs: with epkgs; [
-      treesit-grammars.with-all-grammars
-      vterm
-    ]));
+    package = (
+      pkgs.emacsWithPackagesFromUsePackage {
+        package = pkgs.emacs; # pkgs.emacs-git-pgtk;
+        config = ./config.org;
+        defaultInitFile = true;
+        extraEmacsPackages = epkgs: [
+          epkgs.use-package
+          epkgs.dashboard
+          epkgs.all-the-icons
+          epkgs.doom-themes
+          epkgs.doom-modeline
+          epkgs.vterm
+          epkgs.org-superstar
+          epkgs.projectile
+	  epkgs.flycheck
+	  epkgs.vertico
+          epkgs.orderless
+	  epkgs.marginalia
+	  epkgs.yasnippet
+	  epkgs.general
+
+          #lang
+          epkgs.treesit-grammars.with-all-grammars
+          epkgs.lsp-mode
+          epkgs.nix-ts-mode
+          epkgs.rustic
+        ];
+      }
+    );
   };
 
-  home.packages = with pkgs; [
-    binutils
-    (ripgrep.override { withPCRE2 = true; })
-    gnutls
-    fd
-    imagemagick
-    zstd
-    emacs-all-the-icons-fonts
-  ];
-
-  home = {
-    sessionVariables = {
-      DOOMDIR = "${config.xdg.configHome}/doom";
-      EMACSDIR = "${config.home.homeDirectory}/.emacs.d";
-      DOOMLOCALDIR = "${config.xdg.dataHome}/doom";
-      DOOMPROFILELOADFILE = "${config.xdg.stateHome}/doom-profiles-load.el";
-    };
-
-    # Note! This must correspond to $EMACSDIR
-    sessionPath = ["${config.home.homeDirectory}/.emacs.d/bin"];
-  };
-
-  home.file."${config.home.homeDirectory}/.emacs.d".source = inputs.doom-emacs;
-
-  xdg.configFile."doom" = {
-    source = ./doom;
-    recursive = true;
-  };
-
-
+  xdg.configFile."emacs/ue-colorful.png".source = ./ue-colorful.png;
 }
